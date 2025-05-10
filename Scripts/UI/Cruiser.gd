9c595b27fa7d3381f43159844ebdbcb9a60532bb
Elaktrato <elaktrato@gmail.com>
2025-05-10 21:54:03 +0200

    ✨ starting with the map creation

---
+++ Scripts/UI/Cruiser.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+signal doneMoving
+
+var cruiserTween: Tween = get_tree().create_tween()
+
+func tweenDone() -> void:
+	doneMoving.emit()
+
+func moveTo(posToMoveTo: Vector2) -> void:
+	cruiserTween.tween_property(self, "global_position", posToMoveTo, 0.4).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+	cruiserTween.tween_callback(tweenDone)
+	
+func setPos(posToSetTo: Vector2) -> void:
+	global_position = posToSetTo
