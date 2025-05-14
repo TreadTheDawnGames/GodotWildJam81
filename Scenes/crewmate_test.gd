388abac1c8749d1b243be6a092c6236f7dc13854
Miguel <kingofspadeslm567@gmail.com>
2025-05-13 23:10:15 -0700

    crewmate walks

--- Scenes/crewmate_test.gd
+++ Scenes/crewmate_test.gd
@@ -31,11 +31,10 @@ func _unhandled_input(event: InputEvent) -> void:
 		if event.is_action_pressed("AOS.debug_left_click"):
 			var mousepos = Global.mouse_position_to_local(event, camera)
 			label.text = str(mousepos)
-			var from: Vector2i = _global_to_tilemap(crewmate.position)
-			var to: Vector2i = _global_to_tilemap(mousepos)
-			var path = _pathfinding(from, to)
-			print(path)
-			_create_trail.call_deferred(path)
+			crewmate.walk_to(mousepos)
+			# var path = _pathfinding(from, to)
+			# print(path)
+			# _create_trail.call_deferred(path)
 		pass
 
 
@@ -54,30 +53,3 @@ func _create_trail(path):
 
 func _global_to_tilemap(v: Vector2) -> Vector2i:
 	return tilemap.local_to_map(tilemap.to_local(v))
-
-
-### Depth-First Search ahh pathfinding
-func _pathfinding(from: Vector2i, to: Vector2i) -> Array[Vector2i]:
-	if (tilemap.get_cell_source_id(from) < 0):
-		return []
-	
-	# Prepare the stack
-	var path: Array[Vector2i]
-	var stack: Array[Vector2i]
-	stack.push_back(from)
-
-	# Recursively find a path
-	while !stack.is_empty():
-		var v: Vector2i = stack.pop_back()
-		path.push_back(v)	# Gets added to the line
-		if v == to:
-			return path;
-
-		# Push any of the unvisited cells into the node
-		var surround: Array[Vector2i] = tilemap.get_surrounding_cells(v)
-		var filtered: Array[Vector2i] = surround.filter(func(i): return tilemap.get_cell_source_id(i) >= 0 && !path.has(i))
-		filtered.sort_custom(func(a, b): return a.distance_to(to) > b.distance_to(to))
-		if !filtered.is_empty():
-			filtered.all(func(i): stack.push_back(i); return true)
-
-	return []
--- Scripts/crewmate.gd
+++ Scripts/crewmate.gd
@@ -5,6 +5,7 @@ enum Alliance { ALLIANCE_NONE, ALLIANCE_PLAYER, ALLIANCE_ENEMY }
 
 @export var state_machine: StateMachine
 @export var alliance: Alliance
+@export var navigation_tilemap: TileMapLayer
 var _walk_pos: Vector2
 var _path: Array[Vector2]
 
@@ -32,5 +33,42 @@ func _debug_crewmate_test_changed_navpoint(pos: Vector2) -> void:
 	state_machine.transition("Walk", pos)
 
 
-func check_for_valid_navigation(v: Vector2):
-	pass
+func walk_to(target: Vector2) -> void:
+	var from: Vector2i = navigation_tilemap.local_to_map(navigation_tilemap.to_local(position))
+	var to: Vector2i = navigation_tilemap.local_to_map(navigation_tilemap.to_local(target))
+	var arr: Array[Vector2i] = _pathfinding(from, to)
+	var path: Array[Vector2]
+	for i: Vector2i in arr:
+		path.push_back(navigation_tilemap.map_to_local(i) * navigation_tilemap.scale)
+	
+	print(path)
+	state_machine.transition("Walk", path)
+
+
+### Depth-First Search ahh pathfinding
+#
+func _pathfinding(from: Vector2i, to: Vector2i) -> Array[Vector2i]:
+	if (navigation_tilemap.get_cell_source_id(from) < 0):
+		return []
+	
+	# Prepare the stack
+	var path: Array[Vector2i]
+	var stack: Array[Vector2i]
+	stack.push_back(from)
+
+	# Recursively find a path
+	while !stack.is_empty():
+		var v: Vector2i = stack.pop_back()
+		path.push_back(v)	# Gets added to the line
+		if v == to:
+			path.pop_front()
+			return path;
+
+		# Push any of the unvisited cells into the node
+		var surround: Array[Vector2i] = navigation_tilemap.get_surrounding_cells(v)
+		var filtered: Array[Vector2i] = surround.filter(func(i): return navigation_tilemap.get_cell_source_id(i) >= 0 && !path.has(i))
+		filtered.sort_custom(func(a, b): return a.distance_to(to) > b.distance_to(to))
+		if !filtered.is_empty():
+			filtered.all(func(i): stack.push_back(i); return true)
+
+	return []
