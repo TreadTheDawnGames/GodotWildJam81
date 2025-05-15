cd0fb802f45840490209799e594c8b37e0b495d2
Elaktrato <elaktrato@gmail.com>
2025-05-15 21:47:22 +0200

    Revert "Merge branch 'dev' into SpaceMap"
    This reverts commit f6b1deb2acd48d9eabfb28c5e29aa2f8504d7909, reversing
    changes made to 8d512f7e4169a443660182baf1772cd30b881e04.

--- Scenes/ShipBuilder/shop_slot.gd
+++
@@ -1,7 +0,0 @@
-extends Marker2D
-class_name ShopSlot
-
-@onready var priceText: RichTextLabel = $Panel/RichTextLabel
-
-func setPrice(price : int):
-	priceText.text = "Price: $" + str(price)
--- Scripts/Globals/GlobalPlayerInfo.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-var ThePlayerShip : PlayerShip
-var Money : int
