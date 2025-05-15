8d512f7e4169a443660182baf1772cd30b881e04
Elaktrato <elaktrato@gmail.com>
2025-05-15 21:34:58 +0200

    cleanup

--- Scripts/UI/PitStopPopUp.gd
+++
@@ -1,6 +0,0 @@
-extends CanvasLayer
-
-func _make_custom_tooltip(for_text):
-	var label = Label.new()
-	label.text = for_text
-	return label
--- Scripts/UI/PopupSlot.gd
+++
@@ -1,9 +0,0 @@
-extends PanelContainer
-
-
-func _on_mouse_entered() -> void:
-	Popups.showPitStopPopup(Rect2i( Vector2i(global_position), Vector2i(size) ), '')
-
-
-func _on_mouse_exited() -> void:
-	Popups.hidePitStopPopup()
